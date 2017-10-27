93cee1cc52519a82bbe43123b1d7e1884a4b3b46
Anh mai <lyraducusin@gmail.com>
2017-10-27 08:52:58 -0400

    changes

--- Lab2/script.js
+++ Lab2/script.js
@@ -26,8 +26,10 @@ while(playing){
   enemyHealth -= Math.floor(Math.random() * 2) + 1;
   console.log("My health is " + health);
   console.log("Grant's health is " + enemyHealth);
-  if (health <= 0 || enemyHealth <= 0) {
+  if (enemyHealth <= 0 ) {
+    wins++;
+    enemyHealth=10;
     console.log("You won the fight!");
-   playing = false;
+    console.log(wins++);
  }
 }
